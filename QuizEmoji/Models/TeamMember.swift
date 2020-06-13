a08b2a27e0e83f0148b26b8a68b9fd1dfa0464d8
Zakharov Roman <zakharovroman@hotmail.com>
2020-06-13 11:21:32 +0300

    init

---
+++ QuizEmoji/Models/TeamMember.swift
@@ -0,0 +1,14 @@
+//
+//  TeamMember.swift
+//  QuizEmoji
+//
+//  Created by Роман Захаров on 13.06.2020.
+//  Copyright © 2020 Роман Захаров. All rights reserved.
+//
+
+struct TeamMember {
+    let name: String
+    let email: String
+    let loginGitHub: String
+    let Position: String
+}
