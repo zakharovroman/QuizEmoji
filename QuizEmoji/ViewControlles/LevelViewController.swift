7b6f8e459382ecf61feeb76c8af75249a57ef75f
Zakharov Roman <zakharovroman@hotmail.com>
2020-06-14 13:48:59 +0300

    гард на левел

--- QuizEmoji/ViewControlles/LevelViewController.swift
+++ QuizEmoji/ViewControlles/LevelViewController.swift
@@ -35,6 +35,7 @@ class LevelViewController: UIViewController {
     @IBAction func levelButtonPressed(_ sender: UIButton) {
         guard let levelIndex = levelButtons.firstIndex(of: sender) else { return }
         level = Level.element(at: levelIndex)
+        guard let level = level else { return }
         performSegue(withIdentifier: "categorySegue", sender: nil)
     }
     
