fa36f4a2f85345b3fefb7d7f1813a36dada11f8f
Vladchere <vladchere.ok@mail.ru>
2020-06-14 13:53:48 +0700

    Results tablet done

---
+++ QuizEmoji/ViewControlles/HeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCell.swift
+//  QuizEmoji
+//
+//  Created by Vladislav on 13.06.2020.
+//  Copyright © 2020 Роман Захаров. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+    @IBOutlet var levelLabel: UILabel!
+}
